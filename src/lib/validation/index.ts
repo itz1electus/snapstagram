b6a78c9e54875090844424cd6b11942550961921
itz1electus <ariwalamustafa853@gmail.com>
2024-06-06 18:25:40 +0200

    first commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const SignUpValidation = z.object({
+    name: z.string().min(5, { message: 'Your name is too short!' }),
+    username: z.string().min(5, { message: 'Your username is too short!' }).max(50),
+    email: z.string().email(),
+    password: z.string().min(8, { message: 'Your password needs to be stronger (at least 8 characters)!' }),
+  });
